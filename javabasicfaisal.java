5c906360f8f6f41707b7e174fd50d2aba697498b
faisalakb74 <faisall.akbar@gmail.com>
2021-10-12 16:25:46 +0700

    Push tugas java

---
+++ javabasicfaisal.java
@@ -0,0 +1,19 @@
+public class javabasicfaisal{
+    public static void main (String[] args){
+
+        String  firstname   = "Faisal Akbar";
+        String  lastname    = "Rohmat";
+        int     age         = 22;
+        String  phonenumber = "087717418069";
+        String  address     = "Garut";
+        char    bloodtype   = 'B';
+        String  hobby       = "gangguin Ina";
+
+        System.out.println("Name         = "+firstname + lastname);
+        System.out.println("Age          = "+age+"th");
+        System.out.println("Number Phone = "+phonenumber);
+        System.out.println("Address      = "+address);
+        System.out.println("Blood Type   = "+bloodtype);
+        System.out.println("Hobby        = "+hobby);
+    }
+}
